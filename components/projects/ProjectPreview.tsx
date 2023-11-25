558488d1fdfad15ead61c78bdb771c670ade1d26
Jacob Ehlert <jacobdukc@gmail.com>
2023-11-24 21:35:19 -0800

    changed aspect ratio for project card

--- components/projects/ProjectPreview.tsx
+++ components/projects/ProjectPreview.tsx
@@ -17,7 +17,7 @@ export default function ProjectPreview(props: ProjectMetadata) {
         <Tilt options={tiltOptions}>
             <Link
                 href={`/projects/${props.slug}`}
-                className="bg-cover bg-center bg-clip-padding w-full h-auto aspect-[3/1.7] flex flex-col place-content-end align-bottom place-items-end p-32 rounded-out border border-stroke-1 hover:scale-[1.05] active:scale-[.75] duration-150 max-[540px]:p-24"
+                className="bg-cover bg-center bg-clip-padding w-full h-auto aspect-[3/1.7] max-[500px]:aspect-[1.7/3] flex flex-col place-content-end align-bottom place-items-end p-32 rounded-out border border-stroke-1 hover:scale-[1.05] active:scale-[.75] duration-150 max-[540px]:p-24"
                 style={{backgroundImage: `linear-gradient(to bottom,rgba(0,0,0,0),rgba(0,0,0,0.65)), url(${props.image})`}}
             >
                 <h3 className="text-3 w-full font-medium max-[540px]:text-body">{props.title}</h3>
