de1e23e050677abfc08594407badf581489008e4
dukcc <dukkcc@proton.me>
2024-02-21 16:52:14 -0800

    changed around nav

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -14,13 +14,17 @@ export default function Nav() {
       opacity: 1,
       transition: {
         staggerChildren: 0.15,
+        staggerDirection: -1,
       },
     },
   };
-  const childVars = {
-    hidden: { opacity: 0, x: 20 },
-    show: { opacity: 1, x: 0 },
-  };
+  const navItems = [
+    { text: "Home", link: "/" },
+    { text: "Projects", link: "/#projects" },
+    { text: "Blog", link: "/blogs" },
+    { text: "GitHub", link: "https://github.com/DuckyHQ0" },
+    { text: "Alive", link: "https://alive.duckyhq.com" },
+  ];
   return (
     <nav className="fixed top-48 max-[630px]:top-24 place-self-center z-50 flex flex-col gap-12 max-w-[1300px] w-full mx-20">
       <div className="px-128 py-16 flex max-[630px]:px-32 w-full z-50 place-content-between place-items-center backdrop-blur-main bg-bg-1 border border-stroke-1 rounded-out max-[1300px]:border-x-0 max-[1300px]:rounded-none">
@@ -33,41 +37,19 @@ export default function Nav() {
             className="hover-active-effect"
           />
         </Link>
-        <div className="flex gap-32 max-[630px]:gap-16 max-[700px]:hidden">
-          <Link
-            href={"/"}
-            className="text-sub hover:text-selected active:text-selected/75 duration-150"
-          >
-            Home
-          </Link>
-          <Link
-            href={"/#projects"}
-            className="text-sub hover:text-selected active:text-selected/75 duration-150"
-          >
-            Projects
-          </Link>
-          <Link
-            href={"/blogs"}
-            className="text-sub hover:text-selected active:text-selected/75 duration-150"
-          >
-            Blog
-          </Link>
-          <Link
-            href={"https://github.com/DuckyHQ0"}
-            className="text-sub hover:text-selected active:text-selected/75 duration-150"
-          >
-            GitHub
-          </Link>
-          <Link
-            href={"https://alive.duckyhq.com"}
-            className="text-sub hover:text-selected active:text-selected/75 duration-150"
-          >
-            Alive
-          </Link>
-        </div>
+        <motion.div
+          initial="hidden"
+          whileInView="show"
+          variants={containerVars}
+          className="flex gap-32 max-[630px]:gap-16 max-[700px]:hidden"
+        >
+          {navItems.map((link, index) => {
+            return <NavLink text={link.text} link={link.link} key={index} />;
+          })}
+        </motion.div>
         <button
           onClick={() => setNavbar(!navbar)}
-          className="hidden text-[24px] max-[700px]:block"
+          className="hidden text-[28px] max-[700px]:block"
         >
           {navbar ? <HiXMark /> : <HiBars3 />}
         </button>
@@ -80,32 +62,39 @@ export default function Nav() {
           navbar ? "flex" : "hidden"
         }`}
       >
-        <motion.div variants={childVars}>
-          <MobileNavLink text="Home" link="/" />
-        </motion.div>
-        <motion.div variants={childVars}>
-          <MobileNavLink text="Projects" link="/#projects" />
-        </motion.div>
-        <motion.div variants={childVars}>
-          <MobileNavLink text="Blog" link="/blogs" />
-        </motion.div>
-        <motion.div variants={childVars}>
-          <MobileNavLink text="GitHub" link="https://github.com/DuckyHQ0" />
-        </motion.div>
-        <motion.div variants={childVars}>
-          <MobileNavLink text="Alive" link="https://alive.duckyhq.com" />
-        </motion.div>
+        {navItems.map((link, index) => {
+          return (
+            <NavLink textBig text={link.text} link={link.link} key={index} />
+          );
+        })}
       </motion.div>
     </nav>
   );
 }
-const MobileNavLink = ({ text, link }: { text: string; link: string }) => {
+
+const childVars = {
+  hidden: { opacity: 0, x: 20 },
+  show: { opacity: 1, x: 0 },
+};
+const NavLink = ({
+  text,
+  link,
+  textBig,
+}: {
+  text: string;
+  link: string;
+  textBig?: boolean;
+}) => {
   return (
-    <Link
-      href={link}
-      className="text-3 hover:text-selected active:text-selected/75 duration-150"
-    >
-      {text}
-    </Link>
+    <motion.div variants={childVars}>
+      <Link
+        href={link}
+        className={`hover:text-selected active:text-selected/75 duration-150 ${
+          textBig ? "text-3" : "text-sub"
+        }`}
+      >
+        {text}
+      </Link>
+    </motion.div>
   );
 };
